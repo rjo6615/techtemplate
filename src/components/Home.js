58914b2d4758706412381740426193194fd250b6
rjo6615 <robertjobernier@gmail.com>
2023-09-26 14:03:57 -0400

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <div>
+            <img style={{width: "100%", height: "500px"}} src="./images/techcubes.jpg" alt="techcubes"></img>
+        </div> 
+   )
+}
+
+export default Home;
